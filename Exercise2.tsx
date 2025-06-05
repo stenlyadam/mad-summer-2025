81c61b7b964cb39be9c5e5d1ffd475a96b114391
stenlyadam <stenly.adam@unklab.ac.id>
2025-06-05 11:20:09 +0800

    adding example exercise2

---
+++ Exercise2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Exercise2 = () => {
+  return (
+    <View>
+      <Text>Exercise 2</Text>
+    </View>
+  );
+};
+
+export default Exercise2;
