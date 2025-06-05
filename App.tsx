38eb36797800a28b02ffc249f4cb8bd5f5a198e3
stenlyadam <stenly.adam@unklab.ac.id>
2025-06-05 10:51:42 +0800

    image from base64

--- App.tsx
+++ App.tsx
@@ -14,6 +14,12 @@ const App = () => {
         style={style.img1}
         source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
       />
+      <Image
+        style={style.img1}
+        source={{
+          uri: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+      />
     </View>
   );
 };
