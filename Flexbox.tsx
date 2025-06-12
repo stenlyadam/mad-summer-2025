e9acd872ca281666e7e2a61f6f31a7af2b6aab76
stenlyadam <stenly.adam@unklab.ac.id>
2025-06-12 10:57:18 +0800

    flexbox

---
+++ Flexbox.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Flexbox = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.redContainer} />
+      <View style={styles.greenContainer} />
+      <View style={styles.blueContainer} />
+    </View>
+  );
+};
+
+export default Flexbox;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'yellow',
+    flex: 1,
+    flexDirection: 'row', //atur main axis
+    justifyContent: 'space-between', //main axis
+    alignItems: 'center', //cross axis
+  },
+  redContainer: {
+    backgroundColor: 'red',
+    height: 100,
+    width: 100,
+  },
+  greenContainer: {
+    backgroundColor: 'green',
+    height: 100,
+    width: 100,
+  },
+  blueContainer: {
+    backgroundColor: 'blue',
+    height: 100,
+    width: 100,
+  },
+});
--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Exercise2 from './Exercise2';
+
 import {name as appName} from './app.json';
 import Login from './Login';
+import Flexbox from './Flexbox';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => Flexbox);
