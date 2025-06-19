a35b1e0a6ed2de8314cb3459aa8adeae2a4b241d
stenlyadam <stenly.adam@unklab.ac.id>
2025-06-19 11:57:34 +0800

    slicing sign up page

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import SplashScreen from './src/pages/SplashScreen';
 import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
 const App = () => {
-  return <SignIn />;
+  return <SignUp />;
 };
 
 export default App;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,60 @@
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Header, TextInput} from '../../components/molecules';
+import {Button, Gap} from '../../components/atoms';
+import {NullPhoto} from '../../assets';
+
+const SignUp = () => {
+  return (
+    <View style={styles.pageContainer}>
+      <Header text="Sign Up" />
+      <View style={styles.contentContainer}>
+        <View style={styles.profileContainer}>
+          <View style={styles.profileBorder}>
+            <TouchableOpacity activeOpacity={0.5}>
+              <Image source={NullPhoto} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Gap height={26} />
+        <TextInput text="Full Name" placeholder="Enter your full name" />
+        <Gap height={26} />
+        <TextInput
+          text="Email Address"
+          placeholder="Enter your email address"
+        />
+        <Gap height={16} />
+        <TextInput text="Password" placeholder="Enter your password" />
+        <Gap height={24} />
+        <Button text="Continue" />
+        <Gap height={12} />
+      </View>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    marginTop: 24,
+    marginHorizontal: 24,
+  },
+  profileContainer: {
+    alignItems: 'center',
+  },
+  profileBorder: {
+    height: 110,
+    width: 110,
+    borderColor: '#8D92A3',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 110 / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
