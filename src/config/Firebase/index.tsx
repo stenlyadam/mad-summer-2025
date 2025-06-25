43c414b274bb7e61f03ae5aced65ff011f6eeb6c
stenlyadam <stenly.adam@unklab.ac.id>
2025-06-25 10:42:41 +0800

    adding firebase configuration file

---
+++ src/config/Firebase/index.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCS3-GgVXLYJ7lw6Y1AgZBT-Su2JbbRI5g',
+  authDomain: 'mtdemosummer2025.firebaseapp.com',
+  projectId: 'mtdemosummer2025',
+  storageBucket: 'mtdemosummer2025.firebasestorage.app',
+  messagingSenderId: '374250257604',
+  appId: '1:374250257604:web:1937864ec1e942ceaa1d91',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export default app;
